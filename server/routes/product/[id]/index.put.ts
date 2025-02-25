e8c4ba9a27fa54c9261c2da7ebf4594887c7c45c
Loïc MARTIN <martin.loic.prod@gmail.com>
2025-02-25 17:04:26 +0100

    feat(api): Implement product update endpoint with XML conversion and error handling

---
+++ server/routes/product/[id]/index.put.ts
@@ -0,0 +1,56 @@
+import { z } from 'zod';
+import { create } from 'xmlbuilder2';
+import { parseStringPromise } from 'xml2js'; // You need to install this package
+
+export default defineEventHandler(async (event) => {
+  try {
+    const body = await readBody(event);
+    const parsedData = productSchema.parse(body);
+
+    const prestashopApiUrl = process.env.PRESTASHOP_URL+'/api/product';
+    const apiKey = process.env.PRESTASHOP_API_KEY;
+
+    // Convert JSON to XML using xmlbuilder2
+    const xmlDoc = create({ version: '1.0', encoding: 'UTF-8' });
+    const xmlData = xmlDoc.ele('prestashop').ele(parsedData).end({ prettyPrint: true });
+
+    console.log("Body", body);
+    console.log("Parse", parsedData);
+    console.log("xmlData", xmlData);
+    // return 'Data sent successfully';
+
+    const response = await fetch(prestashopApiUrl, {
+      method: 'PUT',
+      headers: {
+        'Authorization': 'Basic ' + Buffer.from(apiKey + ':').toString('base64'),
+        'Content-Type': 'application/xml',
+      },
+      body: xmlData,
+    });
+
+    if (!response.ok) {
+      const errorText = await response.text();
+      throw new Error(`Failed to send data: ${response.statusText}. Details: ${errorText}`);
+    }
+
+    const responseText = await response.text();
+    const responseData = await parseStringPromise(responseText);
+    return {
+      success: true,
+      data: responseData,
+    };
+  } catch (error) {
+    console.log(error);
+    if (error instanceof z.ZodError) {
+      return {
+        success: false,
+        errors: error.errors,
+      };
+    }
+
+    return {
+      success: false,
+      message: error.message,
+    };
+  }
+});
