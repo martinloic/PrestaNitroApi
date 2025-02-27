e8334fda1af28fe5db6eca7881f8f0b64524483b
Loïc MARTIN <martin.loic.prod@gmail.com>
2025-02-27 16:39:44 +0100

    refactor(category): Move language transformation logic to utility function

--- server/routes/category/index.post.ts
+++ server/routes/category/index.post.ts
@@ -1,33 +1,17 @@
 import { z } from 'zod';
 import { create } from 'xmlbuilder2';
-import { parseStringPromise } from 'xml2js'; // You need to install this package
+import { parseStringPromise } from 'xml2js';
+import { transformLanguages } from '~/utils/transformData';
+import { categorySchema } from '~/utils/schemas/category';
 
 export default defineEventHandler(async (event) => {
   try {
     const body = await readValidatedBody(event, categorySchema.parse);
 
-    // Transformation des langues en un format adapté à xmlbuilder2
-    const transformLanguages = (obj) => {
-      for (const key in obj) {
-        if (typeof obj[key] === 'object' && obj[key] !== null) {
-          if ('language' in obj[key] && Array.isArray(obj[key].language)) {
-            // Convertir chaque "language" en un tableau de balises XML
-            obj[key].language = obj[key].language.map(lang => ({
-              '@id': lang.id,
-              '#text': lang.text
-            }));
-          } else {
-            transformLanguages(obj[key]); // Récursion si l'objet contient d'autres objets
-          }
-        }
-      }
-    };
-
     // Appliquer la transformation sur "category"
     if (body.category) {
       transformLanguages(body.category);
     }
-
 
     const prestashopApiUrl = process.env.PRESTASHOP_URL+'/api/categories';
     const apiKey = process.env.PRESTASHOP_API_KEY;
